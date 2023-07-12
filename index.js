a045c03ba984daac859dcdcb5dc80ff0513aec6b
Agustinjz28 <139351124+Agustinjz28@users.noreply.github.com>
2023-07-12 09:03:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('No me olvide, 3 años ya desde que el destino nos cruzo caminos, y fue lo mejor que hizo la vida por mi')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function () {
+    alert('Te quiero demasiado , gracias por todo y gracias a di maria tambien , fue el causante de este gran amor jaja, te amo mi mariquita') 
+})
